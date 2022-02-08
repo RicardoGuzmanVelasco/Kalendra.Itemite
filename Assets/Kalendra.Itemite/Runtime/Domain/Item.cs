479c22be71d367c11d6b07de93020afe4801a7e2
RGV <ricardo.guzman.velasco@outlook.com>
2022-02-08 21:59:12 +0100

    feat(domain): add item with tags
    Add some control cases tests: no empty tags, ignore repeated
    tags...
    Show first items relationships notion: disjoint tags, overlapping
    tags.

---
+++ Assets/Kalendra.Itemite/Runtime/Domain/Item.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace Kalendra.Itemite.Runtime.Domain
+{
+    public class Item
+    {
+        public string Name { get; }
+        public IEnumerable<string> Tags { get; }
+
+        public Item(string name, [NotNull] IEnumerable<string> tags)
+        {
+            if(!tags.Any())
+                throw new ArgumentException();
+
+            Name = name;
+            Tags = new HashSet<string>(tags);
+        }
+
+        public float RelateWith(Item other)
+        {
+            var allTags = Tags.Union(other.Tags);
+            var commonTags = Tags.Intersect(other.Tags);
+            
+            return commonTags.Count()/(float)allTags.Count();
+        }
+    }
+}
--- Assets/Kalendra.Itemite/Tests/ItemRelationshipsTests.cs
+++ Assets/Kalendra.Itemite/Tests/ItemRelationshipsTests.cs
@@ -1,7 +1,62 @@
-namespace Tests
+using System;
+using System.Linq;
+using FluentAssertions;
+using Kalendra.Itemite.Runtime.Domain;
+using NUnit.Framework;
+
+namespace Kalendra.Itemite.Tests
 {
     public class ItemRelationshipsTests
     {
+        [Test]
+        public void Item_Creation()
+        {
+            var sut = new Item("Tree", new[]{ "Plant", "Natural" });
+
+            sut.Name.Should().Be("Tree");
+            sut.Tags.Should().BeEquivalentTo("Plant", "Natural");
+        }
+
+        [Test]
+        public void Item_CannotHave_EmptyTags()
+        {
+            Action act = () => new Item("Empty", Enumerable.Empty<string>());
+
+            act.Should().Throw<ArgumentException>();
+        }
         
+        [Test]
+        public void Item_CannotHave_NullTags()
+        {
+            Action act = () => new Item("Empty", null);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void Item_Ignores_RepeatedTags()
+        {
+            var sut = new Item("Tree", new[] { "Plant", "Plant" });
+
+            sut.Tags.Should().HaveCount(1);
+        }
+
+        [Test]
+        public void Relating_ItemsWithSomeCommonTag_IsNotZero()
+        {
+            var doc = new Item("Home", new[]{ "Shelter, Human" });
+            var sut = new Item("Tree", new[]{ "Plant", "Natural" });
+
+            sut.RelateWith(doc).Should().BeApproximately(0, float.Epsilon);
+        }
+
+        [Test]
+        public void Relating_DisjointItems_IsAroundZero()
+        {
+            var doc = new Item("Fire", new[]{ "Natural" });
+            var sut = new Item("Tree", new[]{ "Plant", "Natural" });
+
+            sut.RelateWith(doc).Should().BePositive();
+        }
     }
 }
