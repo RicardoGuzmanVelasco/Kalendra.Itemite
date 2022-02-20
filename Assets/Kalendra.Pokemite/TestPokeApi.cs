242dad3be42378324b26d7941d0a5cbe18005fb5
RGV <ricardo.guzman.velasco@outlook.com>
2022-02-20 13:40:23 +0100

    feat(Pokemon): add PokeApi texture retrieving

--- Assets/Kalendra.Pokemite/TestPokeApi.cs
+++ Assets/Kalendra.Pokemite/TestPokeApi.cs
@@ -1,19 +1,36 @@
+using System.Collections;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using PokeApiNet;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace Kalendra.Pokemite
 {
     public class TestPokeApi : MonoBehaviour
     {
         readonly PokeApiClient client = new PokeApiClient();
-        
+
         async void Start()
         {
             var pikachu = await AwaitForPikachu();
 
-            Debug.Log(pikachu.Sprites.FrontDefault);
+            StartCoroutine(LoadSprite(pikachu.Sprites.FrontDefault));
+        }
+
+        IEnumerator LoadSprite(string url)
+        {
+            using var request = UnityWebRequestTexture.GetTexture(url);
+
+            yield return request.SendWebRequest();
+
+            var texture = DownloadHandlerTexture.GetContent(request);
+
+            // render.sprite = SpriteFrom(texture);
+        }
+
+        Sprite SpriteFrom(Texture2D texture)
+        {
+            return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one * .5f, 100f);
         }
 
         async Task<Pokemon> AwaitForPikachu()
