af1cf8d4d50b0ee5f0b4a7bda84f7a5b2e966f8f
RGV <ricardo.guzman.velasco@outlook.com>
2022-02-27 14:33:54 +0100

    feat(Pokemon domain): add base exp and types to relation heuristic

--- Assets/Kalendra.Pokemite/Runtime/Domain/RelatablePkmn.cs
+++ Assets/Kalendra.Pokemite/Runtime/Domain/RelatablePkmn.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using System.Numerics;
 using PokeApiNet;
 
 namespace Kalendra.Pokemite.Runtime.Domain
@@ -13,12 +16,27 @@ namespace Kalendra.Pokemite.Runtime.Domain
 
         public float RelateWith(RelatablePkmn other)
         {
-            return RelationHeuristic(this, other);
+            return RelationHeuristic(this.pkmn, other.pkmn);
         }
 
-        static float RelationHeuristic(RelatablePkmn pkmn1, RelatablePkmn pkmn2)
+        static float RelationHeuristic(Pokemon p1, Pokemon p2)
         {
-            return pkmn1.pkmn.Height - pkmn2.pkmn.Height;
+            return ByBaseExp(p1, p2) + ByTypes(p1, p2);
+        }
+
+        static float ByBaseExp(Pokemon p1, Pokemon p2)
+        {
+            var baseExpRange = new Vector2(20, 255);
+            var baseExpDifference = Math.Abs(p1.BaseExperience - p2.BaseExperience);
+
+            return baseExpRange.Length() - baseExpDifference;
+        }
+
+        static float ByTypes(Pokemon p1, Pokemon p2)
+        {
+            const int typesCount = 18;
+            return p1.Types.Intersect(p2.Types).Count() * typesCount;
+            //TODO: bonus if slots match (slots = type is primary, secondary...)
         }
     }
 }
