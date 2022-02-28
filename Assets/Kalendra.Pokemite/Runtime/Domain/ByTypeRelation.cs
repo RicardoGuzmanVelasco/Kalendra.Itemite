7779cf2c79b3d92aafb4d54b50c081933940ec1c
RGV <ricardo.guzman.velasco@outlook.com>
2022-02-28 09:57:57 +0100

    refactor(Pokemon): add support methods when getting type relation

--- Assets/Kalendra.Pokemite/Runtime/Domain/ByTypeRelation.cs
+++ Assets/Kalendra.Pokemite/Runtime/Domain/ByTypeRelation.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Annotations;
 using PokeApiNet;
 
 namespace Kalendra.Pokemite.Runtime.Domain
@@ -7,13 +9,31 @@ namespace Kalendra.Pokemite.Runtime.Domain
     {
         public float Relate(Pokemon p1, Pokemon p2)
         {
-            var types1 = p1.Types.Select(t => t.Type.Name);
-            var types2 = p2.Types.Select(t => t.Type.Name);
+            var types1 = TypeNamesFrom(p1);
+            var types2 = TypeNamesFrom(p2);
 
-            var common = types1.Intersect(types2).Count();
-            var unCommon = types1.Union(types2).Count() - common;
+            var common = CommonTypes(types1, types2);
+            var unCommon = UncommonTypes(types1, types2);
 
             return common / (unCommon + 1f);
+        }
+
+        [Pure]
+        static int UncommonTypes(IEnumerable<string> types1, IEnumerable<string> types2)
+        {
+            return types1.Union(types2).Count() - CommonTypes(types1, types2);
+        }
+
+        [Pure]
+        static int CommonTypes(IEnumerable<string> types1, IEnumerable<string> types2)
+        {
+            return types1.Intersect(types2).Count();
+        }
+
+        [Pure]
+        static IEnumerable<string> TypeNamesFrom(Pokemon pkmn)
+        {
+            return pkmn.Types.Select(t => t.Type.Name);
         }
     }
 }
